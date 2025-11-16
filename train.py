6b7956ac4f0c80583dc28f82fc596682905be756
Atharv Chandorikar <atharv.chandorikar@gmail.com>
2025-11-17 01:14:39 +0530

    yolo model trained on roboflow dataset 50 epochs batch size 8  workers 0

--- train.py
+++ train.py
@@ -1,8 +1,14 @@
 from ultralytics import YOLO
+import torch
 
-model = YOLO('yolo11m.pt')
 
-data_yaml = 'data/IntelligentVision-7/data.yaml'
+if __name__ == '__main__':
+    # Set multiprocessing start method for Windows
+    torch.multiprocessing.set_start_method('spawn', force=True)
+    
+    model = YOLO('yolo11m.pt')
 
-results = model.train(data=data_yaml, epochs = 50, imgsz = 640, batch = 16, name = 'trash_model', patience = 10, save =True, plots = True)
+    data_yaml = 'data/IntelligentVision-7/data.yaml'
+
+    results = model.train(data=data_yaml, workers=0, epochs=50, imgsz=640, batch=8, name='trash_model', patience=10, save=True, device=0, plots=True)
 
