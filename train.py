ae8558a03c0fb96a8cf76e04c89b58cb6801ee74
the25thjin <mannr075@gmail.com>
2025-11-20 19:25:31 +0530

    requirement file added

--- train.py
+++ train.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     
     model = YOLO('yolo11m.pt')
 
-    data_yaml = 'data/merged_chokho_dataset'
+    data_yaml = 'data/merged_chokho_dataset/data.yaml'
 
-    results = model.train(data=data_yaml, workers=0, epochs=100, imgsz=640, batch=16, name='trash_model', patience=20, augment=True, mosaic=1.0, mixup=0.15, degrees=10, flipud=0.5, fliplr=0.5, save=True, device=0, plots=True)
+    results = model.train(data=data_yaml, workers=0, epochs=100, imgsz=640, batch=16, name='trash_model', patience=20, augment=True, mosaic=1.0, mixup=0.15, degrees=10, flipud=0.5, fliplr=0.5, save=True, plots=True)
 
 print(f"Best mAP50: {results.results_dict['metrics/mAP50(B)']:.3f}")
