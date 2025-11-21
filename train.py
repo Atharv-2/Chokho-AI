69a4da3ad1d40d8676119224e24cb54f359d0d2e
Atharv Chandorikar <atharv.chandorikar@gmail.com>
2025-11-21 12:48:16 +0530

    trained model on 100 epochs 16 batch size nano model

--- train.py
+++ train.py
@@ -3,13 +3,24 @@ import torch
 
 
 if __name__ == '__main__':
-    # Set multiprocessing start method for Windows
     torch.multiprocessing.set_start_method('spawn', force=True)
     
-    model = YOLO('yolo11m.pt')
-
+    model = YOLO('yolo11n.pt')
+    
     data_yaml = 'data/merged_chokho_dataset/data.yaml'
-
-    results = model.train(data=data_yaml, workers=0, epochs=100, imgsz=640, batch=16, name='trash_model', patience=20, augment=True, mosaic=1.0, mixup=0.15, degrees=10, flipud=0.5, fliplr=0.5, save=True, plots=True)
-
-print(f"Best mAP50: {results.results_dict['metrics/mAP50(B)']:.3f}")
+    
+    results = model.train(
+        data=data_yaml,
+        workers=0,
+        epochs=100,
+        imgsz=640,
+        batch=16,
+        name='trash_model_1',
+        patience=20,
+        device=0,
+        plots=True,
+        save=True,
+        exist_ok=True
+    )
+    
+    print(f"Training complete!")
